2aaf0fd8736b84a9f2cb0bc377f391215dd683e3
Arthur L <arthurifccl@gmail.com>
2019-11-13 00:23:00 -0300

    Edits

--- chat/servidor/index.js
+++ chat/servidor/index.js
@@ -2,20 +2,26 @@ var io = require('socket.io')(8090);
 
 io.on('connection', (socket) => {
     
-    console.log(socket.id + ' conectado, aguardando transmissão de chave publica.')
+    console.log(socket.id + ' conectado')
+    socket.broadcast.emit('chave-publica')
 
-    socket.on('chave-publica', (id)=> {
-        
-        socket.broadcast.emit('chave-publica',chave)
+    socket.on('chave-publica', ()=> {
+        socket.broadcast.emit('chave-privada')
     })
 
     socket.on('envia-chave-publica', (chave)=>{
-        socket.broadcast.emit("recebe-chave-publica", {id: socket.id, chave_publica: chave})
+        socket.broadcast.emit("recebe-chave-publica", {id: socket.id, chave_privada: chave})
     })
 
     socket.on('message', (data)=>{
-        socket.broadcast.emit('message', {id: socket.id, msg: data})
+       if (io.sockets.connected[data.id] === undefined){
+            return;
+        }
+        io.sockets.connected[data.id].emit('message', {id: socket.id, msg: data.payload});
     })
 
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('exit', {id: socket.id});
+        console.log(socket.id + ' desconectado.')
+    });
 });
-
